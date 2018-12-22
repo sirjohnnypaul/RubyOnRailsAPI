568ffff02bdb6a5b7887e080d510ddead62efe69
sirjohnnypaul <jan.szczesniewicz@icloud.com>
2018-12-22 16:23:15 +0100

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :members
-  resources :tracks
-  resources :albums
-  resources :bands
+  namespace 'api' do
+    namespace 'v1' do
+      resources :users
+      resources :members
+      resources :tracks
+      resources :albums
+      resources :bands
 
-  get 'albums_by_band_id/band_id' => 'albums_by_band_id/#index'
-  get 'tracks_by_album_id/album_id' => 'tracks_by_album_id/#index'
+      get 'albums_by_band_id/band_id' => 'albums_by_band_id/#index'
+      get 'tracks_by_album_id/album_id' => 'tracks_by_album_id/#index'
 
-  resources :albums_by_band_id, param: :band_id
-  resources :tracks_by_album_id, param: :album_id
+      resources :albums_by_band_id, param: :band_id
+      resources :tracks_by_album_id, param: :album_id
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+      # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    end
+  end
 end
