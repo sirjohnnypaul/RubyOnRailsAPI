41e189b22dbb8e1c67c3a07656e0101a1a9dfee7
sirjohnnypaul <jan.szczesniewicz@icloud.com>
2018-12-24 00:32:06 +0100

    fiftth commit

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,11 @@ Rails.application.routes.draw do
       get 'albums/id' => 'albums/#show'
       get 'albums_by_band_id/band_id' => 'albums_by_band_id/#index'
       get 'tracks_by_album_id/album_id' => 'tracks_by_album_id/#index'
+      get 'bandname_by_band_id/band_id' => 'bandname_by_band_id/#index'
 
       resources :albums_by_band_id, param: :band_id
       resources :tracks_by_album_id, param: :album_id
+      resources :bandname_by_band_id, param: :band_id
 
       # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     end
